def3ff2102eaebcc74dcb77b968df52e4212c831
Mubashir <hussainmuby@gmail.com>
2023-05-18 17:19:59 -0600

    Able to get starting position for pac per lvl

--- src/pacman.py
+++ src/pacman.py
@@ -65,3 +65,26 @@ class PacMan:
     
     def _get_power_up_score(self):
         return self._power_up_score
+    
+    def _get_starting_position_from_current_level(self, map_obj, current_level):
+
+        x = int(map_obj._rows/2)
+        y = int(map_obj._columns/2)
+        
+        if current_level[x][y] == "1":
+            
+            if([x + 1][y + 1] != "1"):
+                self._position = (x+1,y+1)
+            elif ([x + 1][y - 1] != "1"):
+                self._position = (x+1,y-1)
+            elif ([x - 1][y + 1] != "1"):
+                self._position = (x-1,y+1)
+            elif ([x - 1][y - 1] != "1"):
+                self._position = (x-1,y-1)
+            else:
+                print("ERROR: No starting position available")
+                return False 
+            return True
+        else:
+            self._position = (x,y)
+            return True
