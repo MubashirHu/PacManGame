7e66c193634c2905f9124555485e74656b5894b7
Mubashir <hussainmuby@gmail.com>
2023-07-11 17:11:41 -0600

    ad hoc name change

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     myController = Controller()
     myController._initialize_game()
     myController._display_initial_positions()
-    myController._set_initial_states_and_ghost_targets()
+    myController._set_initial_states_and_scatter_targets()
     myController._display_positions_of_pieces_in_view_text()
 
     myController.my_view._root.mainloop()
