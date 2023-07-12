a0c54875783904a0f7cced0b9c27126b63480fd7
Mubashir <hussainmuby@gmail.com>
2023-07-11 21:52:18 -0600

    naming adhoc

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
 
     myController = Controller()
     myController._initialize_model_and_view()
-    myController._display_initial_positions()
+    myController._draw_maze()
     myController._display_coordinates()
 
     myController.my_view._root.mainloop()
