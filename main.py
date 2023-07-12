29bbff89224cc825f9916c59028add9fc70494e1
Mubashir <hussainmuby@gmail.com>
2023-07-12 00:55:12 -0600

    removed functions adhoc

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def main():
 
     myController = Controller()
     myController._initialize_model_and_view()
-    myController._draw_maze()
-    myController._display_coordinates()
 
     myController.my_view._root.mainloop()
     
